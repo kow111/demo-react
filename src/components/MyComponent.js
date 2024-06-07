a99d0955c03c43b5dcd52bdaa39dea620b929446
kow111 <chome710@gmail.com>
2024-06-07 10:34:48 +0700

    f component

---
+++ src/components/MyComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class MyComponents extends React.Component {
+    render() {
+        return (
+            <div> my first component</div>
+        )
+    }
+}
+
+export default MyComponents;
