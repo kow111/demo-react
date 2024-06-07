e558a036ada74e09335c5e19ed56d6a723d50c92
kow111 <chome710@gmail.com>
2024-06-07 16:35:45 +0700

    add state

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 class MyComponents extends React.Component {
-    render() {
-        return (
-            <div> my first component</div>
-        )
-    }
+  state = {
+    name: "Teo",
+    age: 10,
+  };
+  render() {
+    return <div> My name is {this.state.name}</div>;
+  }
 }
 
 export default MyComponents;
