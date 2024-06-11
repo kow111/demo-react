66a9a0eeee1218356e096c6910f87f56300fdb28
kow111 <chome710@gmail.com>
2024-06-11 18:18:06 +0700

    dom event

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,12 +1,23 @@
 import React from "react";
-
 class MyComponents extends React.Component {
   state = {
     name: "Teo",
     age: 10,
   };
+  handleClick() {
+    console.log(this.state.name);
+  }
+  handleOnMoveOver(event) {
+    console.log(event);
+  }
   render() {
-    return <div> My name is {this.state.name}</div>;
+    return (
+      <div>
+        My name is {this.state.name}
+        <button onMouseOver={this.handleOnMoveOver}>hover me</button>
+        <button onClick={this.handleClick}>click me</button>
+      </div>
+    );
   }
 }
 
