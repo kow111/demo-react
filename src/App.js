e740adebd88e043c6c42280d72cc668d38e02c4f
kow111 <chome710@gmail.com>
2024-06-16 13:52:00 +0700

    minor fix

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        Hello world
+        Hello world nhee
         <MyComponents></MyComponents>
       </div>
     );
