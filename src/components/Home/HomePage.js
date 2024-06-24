11af0f35914ff7497295ded607e90b27c7cb03f6
kow111 <chome710@gmail.com>
2024-06-24 11:24:57 +0700

    design homepage

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -6,6 +6,16 @@ const HomePage = () => {
       <video autoPlay muted loop>
         <source src={vid} type="video/mp4" />
       </video>
+      <div className="homepage-content">
+        <div className="title-1">Make forms worth filling out</div>
+        <div className="title-2">
+          Get more data—like signups, feedback, and anything else—with forms
+          designed to be refreshingly different.
+        </div>
+        <div className="title-3">
+          <button>Get started—it's free</button>
+        </div>
+      </div>
     </div>
   );
 };
